17238b4cb5f3050e72ae4464d0861c248ed8ae05
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-22 22:27:31 -0600

    ahhh

---
+++ src/test/java/com/zekepeke/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zekepeke.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
