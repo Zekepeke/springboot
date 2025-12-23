7c6bd727b61f5e7a91718e401e78a80fd1342a36
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-23 00:51:44 -0600

    ahh

--- src/main/java/com/zekepeke/store/OrderService.java
+++ src/main/java/com/zekepeke/store/OrderService.java
@@ -1,7 +1,6 @@
 package com.zekepeke.store;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 // Service (Component) is a general purpose annotation
