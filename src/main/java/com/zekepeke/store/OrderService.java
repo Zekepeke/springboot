4c5a4b81f8aaa702e0de05f121b10e899371b2e7
Esequiel Linares <esequiellinares06@gmail.com>
2025-12-23 00:41:52 -0600

    added stuff

--- src/main/java/com/zekepeke/store/OrderService.java
+++ src/main/java/com/zekepeke/store/OrderService.java
@@ -3,11 +3,18 @@ package com.zekepeke.store;
 public class OrderService {
 
     private PaymentService paymentService;
+
+    public OrderService() {
+
+    }
     public OrderService(PaymentService paymentService) {
         this.paymentService = paymentService;
     }
+
     public void placeOrder() {
-        paymentService.processPayment(10);
+        if (paymentService != null) {
+            paymentService.processPayment(10);
+        }
     }
 
     public void setPaymentService(PaymentService paymentService) {
--- src/main/java/com/zekepeke/store/StoreApplication.java
+++ src/main/java/com/zekepeke/store/StoreApplication.java
@@ -2,12 +2,22 @@ package com.zekepeke.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+        // Instead of manually creating objects and injecting dependencies.
+        // The ioc container takes care of that
+        // ioc container
+        ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
+
+        // raises error, 2 ways to solve this issue:
+        // Using annotations (in the OrderService class in this case)
+        // Using code
+        var orderService = context.getBean(OrderService.class);
+        orderService.placeOrder();
     }
 
 }
